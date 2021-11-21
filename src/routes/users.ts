5e70406042298445bc70973961c9b73be65fcf25
MUHAMMED YASEEN N A <121yaseen@gmail.com>
2021-11-22 00:03:55 +0530

    Basic Routing Added

---
+++ src/routes/users.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+
+const route = Router();
+
+route.get("/", (req, res) => {
+  res.send("GET /users");
+});
+
+route.post("/", (req, res) => {
+  // TEMP register user
+});
+
+route.post("/login", (req, res) => {
+  // TEMP login user
+});
+
+export default route;
