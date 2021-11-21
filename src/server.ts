a6f3d6c9fa30281a676f54c338eda6a36daa6fa4
MUHAMMED YASEEN N A <121yaseen@gmail.com>
2021-11-22 00:00:49 +0530

    Basic Routing Added

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,12 @@
 // since types are added to express, imports will be easy
 import express from "express";
+import userRoute from "./routes/users";
+import urlRoute from "./routes/urls";
 
 const app = express();
+
+app.use("/users", userRoute);
+app.use("/urls", urlRoute);
 
 app.get("/", (req, res) => {
   res.send("Server Running!");
